3c886f141181465d7cb15be7e7dacdeaa9e5e579
Mario Lopez <25378324+mardotio@users.noreply.github.com>
2020-11-15 16:17:04 -0500

    Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from './components/Link/Link';
+import { Tabs } from './components/Tabs/Tabs';
 
 function App() {
+  const [tab, setTab] = useState('test' as string | number);
   return (
     <div className="App">
+      <Tabs value={tab} onChange={v => setTab(v)}>
+        <Link><span>testing</span></Link>
+        <Link>Second</Link>
+        <Link>Third</Link>
+        <Link value="test">Fourth</Link>
+        <Link>Fifth</Link>
+        <Link>Sixth</Link>
+        <Link>Seventh</Link>
+      </Tabs>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
